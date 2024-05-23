9759e8d426cdf3b226f00c564675dc494e66a8dd
RianCahyadi08 <ryancahyadi11@gmail.com>
2024-05-23 20:54:35 +0700

    add to cart to localstorage

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,7 +1,7 @@
 import CardProducts from "../components/Fragments/CardProducts";
 import Button from "../components/Elements/Button/Button";
 import Counter from "../components/Fragments/CounterCart";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Rupiah from "../components/Elements/Formats/Rupiah";
 
 const products = [
@@ -41,18 +41,43 @@ const handleLogout = () => {
 
 const ProductPage = (props) => {
   const [carts, setCarts] = useState([
-    {
-      id: 1,
-      qty: 1,
-    },
+    // {
+    //   id: 1,
+    //   qty: 1,
+    // },
   ]);
 
+  const handleRemoveFromCart = (id) => {
+    setCarts(carts.filter((cart) => cart.id !== id));
+  };
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    setCarts(JSON.parse(localStorage.getItem("carts")) || []);
+  }, []);
+
+  useEffect(() => {
+    if (carts.length > 0) {
+      const sum = carts.reduce((acc, item) => {
+        const product = products.find((product) => product.id == item.id);
+        return acc + product.price * item.qty;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem("carts", JSON.stringify(carts));
+    }
+  }, [carts]);
+
   const handleAddToCart = (id) => {
-    if (carts.find(cart => cart.id === id)) {
-      setCarts(carts.map(cart => cart.id === id ? {...cart, qty: cart.qty + 1} : cart));
+    if (carts.find((cart) => cart.id === id)) {
+      setCarts(
+        carts.map((cart) =>
+          cart.id === id ? { ...cart, qty: cart.qty + 1 } : cart
+        )
+      );
     } else {
       setCarts([
-       ...carts,
+        ...carts,
         {
           id: id,
           qty: 1,
@@ -91,7 +116,9 @@ const ProductPage = (props) => {
           ))}
         </div>
         <div className="basis-1/2">
-          <div className="title-cart mt-5 font-semibold text-3xl text-blue-500">Cart</div>
+          <div className="title-cart mt-5 font-semibold text-3xl text-blue-500">
+            Cart
+          </div>
           {/* <ul className="list-none">
             {carts.map((cart, index) => {
               console.log(cart);
@@ -103,7 +130,7 @@ const ProductPage = (props) => {
               );
             })}
           </ul> */}
-          <table className="table-auto text-left border-separate border-spacing-y-2">
+          <table className="table-auto text-left border-separate">
             <thead>
               <tr>
                 <th>Product</th>
@@ -114,22 +141,43 @@ const ProductPage = (props) => {
             </thead>
             <tbody>
               {carts.map((cart, index) => {
-                const product = products.find((product) => product.id === cart.id);
+                const product = products.find(
+                  (product) => product.id === cart.id
+                );
                 return (
                   <>
                     <tr>
                       <td>{product.name}</td>
-                      <td><Rupiah number={product.price} /></td>
+                      <td>
+                        <Rupiah number={product.price} />
+                      </td>
                       <td>{cart.qty}</td>
-                      <td><Rupiah number={cart.qty * product.price}></Rupiah></td>
+                      <td>
+                        <Rupiah number={cart.qty * product.price}></Rupiah>
+                      </td>
                     </tr>
                   </>
                 );
               })}
+
+              <tr>
+                <td colSpan={3}>
+                  <b>Total Price</b>
+                </td>
+                <td>
+                  <b>
+                    <Rupiah number={totalPrice}></Rupiah>
+                  </b>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
       </div>
+
+      {/* <div className="flex justify-center">
+        <Counter />
+      </div> */}
     </>
   );
 };
